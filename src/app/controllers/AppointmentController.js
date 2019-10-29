498c7f591524c2a41c626958d87a3fcc23f554a6
Paulo Humberto <phumbertodasilva@gmail.com>
2019-10-28 23:19:46 -0300

    Adicionando opção de listar notificações.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -51,13 +51,22 @@ class AppointmentController {
     const { provider_id, date } = req.body;
 
     /**
+     * Check if provider_id is a user_id
+     */
+    if (provider_id === req.userId) {
+      return res
+        .status(401)
+        .json({ error: 'User cannot schedule for himself' });
+    }
+
+    /**
      * Check if provider_id is a provider
      */
-    const isProvider = await User.findOne({
+    const checkisProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
 
-    if (!isProvider) {
+    if (!checkisProvider) {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
